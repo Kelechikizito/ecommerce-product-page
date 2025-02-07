7c2ebf3188be9bcb28f4fa864fa072443c9756d0
Kelechikizito <kaylaychi77@gmail.com>
2025-02-07 19:42:22 +0100

    files update

--- src/assets/components/MainText.jsx
+++ src/assets/components/MainText.jsx
@@ -1,4 +1,6 @@
 import CartIcon from "../images/icon-cart.svg?react";
+import PlusIcon from "../images/icon-plus.svg?react";
+import MinusIcon from "../images/icon-minus.svg?react";
 
 const MainText = () => {
   return (
@@ -14,25 +16,28 @@ const MainText = () => {
 
         <div>
             <div>
-                <p>$125.00<span>50%</span></p>
+                <p className="text-2xl font-extrabold flex gap-4">$125.00<span className="font-semibold text-sm text-[#ff7e1b] px-2 my-2 rounded-md bg-[#ff7e1b1e]">50%</span></p>
             </div>
 
             <div>
-                <p>$250.00</p>
+                <p className="line-through font-sans font-normal leading-[26px] text-[#69707d] text-sm">$250.00</p>
             </div>
         </div>
 
-        <div>
-            <div>
-                <p>
-                    <span>-</span>
+        <div className="flex gap-4">
+            <div className="w-[35%]">
+                <p className="flex justify-between h-full p-4 font-semibold rounded-xl bg-[#f6f8fd]">
+                    <span className="flex justify-center items-center"><MinusIcon /></span>
                         0
-                    <span>+</span>
+                    <span className="flex justify-center items-center"><PlusIcon /></span>
                 </p>
             </div>
 
-            <div>
-                <button><CartIcon /> Add to cart</button>
+            <div className="w-[65%] flex justify-center items-center">
+                <button className="w-[100%] h-full flex justify-center items-center gap-4 font-sans leading-[26px] text-white bg-[#ff7e1b] font-semibold rounded-xl">
+                    <CartIcon className='text-white'/> 
+                    Add to cart
+                </button>
             </div>
         </div>
     </div>
